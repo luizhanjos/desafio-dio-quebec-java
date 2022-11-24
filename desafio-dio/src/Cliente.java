a50367fc83b82ecc232c324a4bfb65df11998429
Luiz Henrique <luizhenrique.anjos10@hotmail.com>
2022-11-24 00:26:35 -0300

    Desafio Criando um Banco Digital

---
+++ desafio-dio/src/Cliente.java
@@ -0,0 +1,13 @@
+
+public class Cliente {
+	
+	private String nomeCliente;
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+}
